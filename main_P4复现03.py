b0d03206465ec5deea34c2dfd5649777285ae9bf
cary910 <709844507@qq.com>
2025-02-25 11:40:58 +0800

    Add files via upload

---
+++ main_P4复现03.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from utils_P4复现01 import qa_agent
+from langchain.memory import ConversationBufferMemory
+
+st.title("智能PDF问答工具")
+with st.sidebar:
+    api_key = st.text_input("请输入您的API密钥：",type="password")
+    st.markdown("[获取API密钥地址](https://bailian.com/?apiKey=1#/apiKey")
+
+if "memory" not in st.session_state:
+    st.session_state["memory"]=ConversationBufferMemory(
+        return_messages=True,
+        memory_key="chat_history",
+        output_key="answer"
+    )
+uploaded_file=st.file_uploader("请上传您的PDF文档",type="pdf")
+question = st.text_input("请对PDF提出问题",disabled = not uploaded_file)
+
+if uploaded_file and question and not api_key:
+    st.info("请输入您的API密钥")
+
+if uploaded_file and question and api_key:
+    with st.spinner("AI正在思考，请稍候..."):
+        response = qa_agent(api_key,st.session_state["memory"],uploaded_file,question)
+        st.write("### 答案")
+        st.write(response["answer"])
+        st.session_state["chat_history"] = response["chat_history"]
+
+    if "chat_history" in st.session_state:
+        with st.expander("历史消息"):
+            for i in range(0,len(st.session_state["chat_history"]),2):
+                human_message=st.session_state["chat_history"][i]
+                ai_message=st.session_state["chat_history"][i+1]
+                st.write(human_message.content)
+                st.write(ai_message.content)
+                if len(st.session_state["chat_history"])-2:
+                    st.divider()
---
+++ utils_P4复现01.py
@@ -0,0 +1,38 @@
+from langchain_openai import  ChatOpenAI
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+
+def qa_agent(api_key,memory,uploaded_file,question):
+    model =ChatOpenAI(
+        model = "qwen-max",
+        api_key=api_key,
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
+    )
+    file_content = uploaded_file.read()
+    temp_file_path = "temp.pdf"
+    with open(temp_file_path, "wb") as temp_file:
+        temp_file.write(file_content)
+    #1、读取，2、定义临时路径和临时名称，3、再打开临时路径 4、然后写入
+    loader = PyPDFLoader(temp_file_path)
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=40,
+        separators=["\n", "。", "！", "？", "，", "、", ""]
+    )
+    texts = text_splitter.split_documents(docs)
+    embedding_model = OpenAIEmbeddings(model = "text-embedding-3-large")
+    db = FAISS.from_documents(texts,embedding_model)
+    retriever = db.as_retriever()
+    qa= ConversationalRetrievalChain.from_llm(
+        llm=model,
+        retriever=retriever,
+        memory=memory
+    )
+    response = qa.invoke({"chat_history": memory,
+                        "question": question})
+    return response
